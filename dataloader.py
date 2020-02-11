0d6fc8a49e094df79acc2835c2f48e485d764c23
nuggy875 <jinugpaeng@gmail.com>
2020-02-11 16:27:13 +0000

    data loading complete -> slow problem

--- dataloader.py
+++ dataloader.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 import os
 import os.path as osp
 import xml.etree.ElementTree as ET
@@ -7,6 +8,13 @@ from torch.utils.data import DataLoader
 from PIL import Image
 
 FILTER_DIFFICULT = True
+class_ = {
+    'person':1.0,
+    'car':2.0,
+    'bus':3.0,
+    'bicycle':4.0,
+    'motorbike':5.0
+}
 
 
 def parse_annotation(path):
@@ -21,14 +29,15 @@ def parse_annotation(path):
         y1 = float(bbox.find('ymin').text) - 1.0
         x2 = float(bbox.find('xmax').text) - 1.0
         y2 = float(bbox.find('ymax').text) - 1.0
-        box = [x1, y1, x2, y2]
-        cls = obj.find('name').text.lower().lower()
+        box = [x1, y1, x2, y2, class_[obj.find('name').text.lower().lower()]]
+        
         difficult = int(obj.find('difficult').text) == 1
         if FILTER_DIFFICULT:
             if not difficult:
-                boxes.append({'cls': cls, 'box': box})
+                # boxes.append({'cls': cls, 'box': box})
+                boxes.append(box)
         else:
-            boxes.append({'cls': cls, 'box': box})
+            boxes.append(box)
     return boxes
 
 
@@ -45,26 +54,34 @@ class RTTS_Dataset(data.Dataset):
         self.format = format
 
     def __getitem__(self, index):
-        print('>>INDEX:', index)
         img_name = self.names[index]
         img_path = osp.join(self.im_dir, (img_name+self.format))
         anno_path = osp.join(self.anno_dir, (img_name+'.xml'))
         bbox = parse_annotation(anno_path)
-        # haze=Image.open(img_path+self.format)
+        image=Image.open(img_path)
 
-        return 
+        img_np = np.array(image)
+        bbox_np = np.array(bbox)
+
+        img_tensor = torch.from_numpy(img_np)
+        bbox_tensor = torch.from_numpy(bbox_np)
+
+        return img_tensor, bbox_tensor
 
     def __len__(self):
         return len(self.names)
 
     def collate_fn(self, batch):
         images = list()
+        bboxes = list()
 
         for b in batch:
-            images.append(b)
+            images.append(b[0])
+            bboxes.append(b[1])
 
         images = torch.stack(images, dim=0)
-        return images
+        bboxes = torch.stack(bboxes, dim=0)
+        return images, bboxes
 
 
 if __name__ == "__main__":
@@ -77,6 +94,8 @@ if __name__ == "__main__":
                                 collate_fn=dataset.collate_fn, shuffle=False)
     #iters_per_epoch = int(len(RTTS_train_loader)/batch_size)
     iters_per_epoch = int(len(RTTS_train_loader))
+    print('ITERS: ', iters_per_epoch)
     data_iter = iter(RTTS_train_loader)
     for step in range(iters_per_epoch):
         data = next(data_iter)
+    print('data loading compelete')
