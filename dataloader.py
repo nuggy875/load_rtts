d23ab68feb70b521dace5127a3fee8ba4e3746df
nuggy875 <jinugpaeng@gmail.com>
2020-02-11 07:58:39 +0000

    len

--- dataloader.py
+++ dataloader.py
@@ -1,13 +1,20 @@
+import os
+import os.path as osp
 import torch.utils.data as data
 from torch.utils.data import DataLoader
 
 class RTTS_Dataset(data.Dataset):
-    def __init__(self, path, train, size):
+    def __init__(self, dataset_dir, train, size):
         super(RTTS_Dataset, self).__init__()
+        self.dest_files = os.path.join(dataset_dir, 'ImageSets', 'Main', 'test.txt')
+        self.im_dir = os.path.join(dataset_dir, 'JPEGImages')
+        self.anno_dir = os.path.join(dataset_dir, 'Annotations')
 
     def __getitem__(self, index):
         print('test')
 
+    def __len__(self):
+        return len(self.im_dir)
 
 
 data_path = '/workspace/data'
